f71c5ec85d0d69bdb6333ae40d0c30c91a2e051e
aswanthkoleri <aswanth9495@gmail.com>
2018-12-14 06:54:52 +0530

    Fix the minor error in mapping

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-var map=new Map();
-map.set(1,"Hello");
-console.log(map.get(1));
+var videoName = new Map();
+videoName.set(1,"5 Years Time")
+videoName.set(2,"Bastard Set Of Dreams")
+videoName.set(3,"Caroline")
+videoName.set(4,"Catwoman_Basketball_Scene")
+videoNo=2;
+var video=videoName.get(videoNo);
+console.log(video)
