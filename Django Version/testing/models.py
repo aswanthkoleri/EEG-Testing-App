e7dcd16e1d5132521e605c6bf031d3eaf8a540d1
aswanthkoleri <aswanth9495@gmail.com>
2018-12-30 10:32:57 +0530

    Remove Django version

--- Django Version/testing/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Sliders(models.Model):
-    slider1 = models.CharField(max_length=200)
-    
--- Django Version/testing/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('start/',views.test,name='test'),
-    path('feedback/',views.feedback,name='feedback'),
-]
